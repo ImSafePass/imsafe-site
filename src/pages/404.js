31f6109f2a12afad7021a0643cb65907adcf3e74
Sasha Klein <sashafklein@gmail.com>
2020-04-12 14:23:56 -0700

    Fixed 404

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,20 +1,24 @@
 import React from "react"
 
-import Layout from "../components/layout"
 import SEO from "../components/seo"
 import { Link } from "gatsby"
 
 const NotFoundPage = () => (
-  <Layout>
+  <>
     <SEO title="404: Not found" />
-    <div className="page bg--royal-blue">
-      <h1>Not Found</h1>
-      <p>
-        You just hit a route that doesn't exist.
-        <Link to="/"> Return home.</Link>
-      </p>
+    <div
+      className="flex flex-col flex-jc--c flex-ai--c bg--royal-blue relative"
+      style={{ height: "calc(100vh - 160px)" }}
+    >
+      <div className="flex flex-col flex-jc--c flex-ai--c color--white">
+        <h1 className="mb30 mt50--neg">Not Found</h1>
+        <h4 className="mb20">You just hit a route that doesn't exist.</h4>
+        <h4>
+          <Link to="/"> Return home.</Link>
+        </h4>
+      </div>
     </div>
-  </Layout>
+  </>
 )
 
 export default NotFoundPage
